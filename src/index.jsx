bb032316afc6cce4b04d10712fef26d90727fc6d
Dan Zhang <zhang.dan4@northeastern.edu>
2023-12-10 11:07:32 -0800

    v1

---
+++ src/index.jsx
@@ -0,0 +1,35 @@
+import { Preloader } from '@/components/common';
+import 'normalize.css/normalize.css';
+import React from 'react';
+import { render } from 'react-dom';
+import 'react-phone-input-2/lib/style.css';
+import { onAuthStateFail, onAuthStateSuccess } from '@/redux/actions/authActions';
+import configureStore from '@/redux/store/store';
+import '@/styles/style.scss';
+import WebFont from 'webfontloader';
+import App from './App';
+import firebase from '@/services/firebase';
+
+WebFont.load({
+  google: {
+    families: ['Tajawal']
+  }
+});
+
+const { store, persistor } = configureStore();
+const root = document.getElementById('app');
+
+// Render the preloader on initial load
+render(<Preloader />, root);
+
+firebase.auth.onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(onAuthStateSuccess(user));
+  } else {
+    store.dispatch(onAuthStateFail('Failed to authenticate'));
+  }
+  // then render the app after checking the auth state
+  render(<App store={store} persistor={persistor} />, root);
+});
+
+
